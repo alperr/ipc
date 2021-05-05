81e2ff32f994972797e5efa16c51a8974b01d7ac
alperr <alper3017@gmail.com>
2021-05-05 23:11:42 +0300

    added website

--- example/test.js
+++
@@ -1,6 +0,0 @@
-var IPC = require("./ipc.js");
-IPC.start("./path-of-binary.exe");
-IPC.onmessage = function()
-{
-	IPC.send("test message");
-}
--- ipc.h
+++
@@ -1,4 +0,0 @@
-#include <stdint.h>
-
-void ipc_send(uint8_t* message , uint32_t size);
-void ipc_listen(int32_t argc, int8_t* argv[], void (*onmsg)(uint8_t* msg, uint32_t size));
